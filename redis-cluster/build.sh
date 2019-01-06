b13d6c74f3885c453879680b10ea84974f6c064d
zxslinux <zhangxingshi@aliyun.com>
2019-01-06 16:36:20 +0800

    redis-cluster playbook

---
+++ redis-cluster/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+set -xe
+PROJECT_ROOT=`cd $(dirname $0);pwd`
+VERSION=$1
+
+# download redis src package
+VERSION=${VERSION:-"stable"}
+wget http://download.redis.io/releases/redis-${VERSION}.tar.gz
+
+# complie redis
+tar xvf redis-${VERSION}.tar.gz
+cd ${PROJECT_ROOT}/redis-${VERSION}
+make
+
+# copy binary file
+cp -a src/{redis-sentinel,redis-benchmark,redis-server,redis-trib.rb,mkreleasehdr.sh,redis-check-aof,redis-check-rdb,redis-cli} ${PROJECT_ROOT}/playbook/roles/redis-cluster/files/
